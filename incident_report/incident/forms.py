4257fc1c84ae66a901fb3a0c1bb7bb2b3934b945
Jose-dev-sys <mendezbenitezjosemanuel@gmail.com>
2025-03-18 22:09:10 -0600

    create inciden view

--- incident_report/incident/forms.py
+++ incident_report/incident/forms.py
@@ -7,3 +7,10 @@ class IncidentForm(forms.ModelForm):
         model = Incident
         fields = ['incident_type', 'incident_category', 'incident_description', 'incident_location', 'incident_image']
 
+        widgets = {
+            'incident_type': forms.TextInput(attrs={'class': 'form-control form-control-lg form-control-solid', 'placeholder': 'Incident title'}),
+            'incident_location': forms.TextInput(attrs={'class': 'form-control form-control-lg form-control-solid', 'placeholder': 'Location of the incident'}),
+            'incident_description': forms.Textarea(attrs={'class': 'form-control form-control-solid', 'placeholder': 'Description', 'rows': 3}),
+            'incident_image': forms.ClearableFileInput(attrs={'class': 'form-control card h-100 w-100 flex-center bg-light-primary border-primary border border-dashed p-8' }),
+            'incident_category': forms.Select(attrs={'class': 'form-select form-select-solid form-select-lg fw-bold','data-control':'select2'}),
+        }
